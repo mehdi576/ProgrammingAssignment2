316c1723265a86df05cc84e15f00142d112e3663
Mehdi <mehdi_sadeghi@rocketmail.com>
2017-10-14 17:02:38 +0330

    The Second Assignment of R programming

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+invr <- NULL                 ##setting the invr as NULL
+set <- function (y) {        ##defining the set function
+	x <<- y              ##value of matrix in parent
+	invr <<- NULL        ##if new matrix,reset to NULL
+}
 
+get <- function() x          ##defining get function - returning value of mat arg
+
+setinverse <- function(inverse) invr <<- inverse  ##assigning value of invr
+getinverse <- function() invr                     ##getting value of invr
+list(set = set, get = get, setinverse = setinverse, getinverse = getinverse) ##for referring to the function with $operator
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+invr <- x$getinverse()
+if(!is.null(invr)) {
+	message("getting cached data")
+	return(inv)
+}
+data <- x$get()
+invr <- solve(data, ...)
+x$setinverse(invr)
+invr
 }
